abf0fb9b8173af0f464f2854ce2d375e748c4104
Se Min <se4@pdx.edu>
2020-02-29 16:14:18 -0800

    First commit

--- cache.py
+++ cache.py
@@ -10,45 +10,66 @@ swaprequest = 0;
 swap = 0; 
 i = 0
 
-n_sets = 15;		
-n_ways = 3;		# maximum of 3 (value of 8)
-n_linesize = 6;		# min: 5 max: 7 (value of 128)
+n_sets = 0;		
+n_ways = 0;		# maximum of 3 (value of 8)
+n_linesize = 0;		# min: 5 max: 7 (value of 128)
 
 numtrace = 3;	# number of traces or test cases
 
 # Replacement policies
 replacement_policy = 0; # True LRU = 0 | 1-bit LRU = 1
 
-# Parameters
+# Parameters inputed by the user 
 num_sets = 2**n_sets;	
 num_ways = 2**n_ways;
 line_size = 2**n_linesize; 
 
-# Checking if parameters are within certain bounds 
+num_sets = input("Enter number of sets: ")
+n_sets = math.log(num_sets, 2)
+print(n_sets)
+
+num_ways = input("Enter ways association: ")
+n_ways = math.log(num_ways,2)
+print(n_ways)
+
 if num_ways > 8:
 	print("ERROR: associativity cannot be greater than 8")
 	exit()
 
-elif line_size > 128 or line_size < 32:
+line_size = input("Enter line size: ")
+n_linesize = math.log(line_size,2)
+print(n_linesize)
+
+if line_size > 128 or line_size < 32:
 	print("ERROR: line size must be between 32 and 128")
 	exit() 
 
+replacement_policy = input("input a 0 for True LRU and a 1 for 1-bit LRU: ")
+if(replacement_policy != 0 and replacement_policy != 1):
+	print("ERROR: inputs are outside of specified bounds")
+	exit()
+
+
 # functions: getting how many bits belong in each field 
+# Currently only showing 8 bits and should show 32 bits 
 def get_taglength(address):
 	global taglength
-	taglength = len(address) - n_sets - n_linesize
-	print(len(address))
-	print(taglength)
+	taglength = 32 - n_sets - n_linesize
 
 def get_index(address):
 	# of cache blocks per set: line_size
 	global indexlength
 	indexlength = n_sets - n_ways
-	print(indexlength)
+#	print(indexlength)
 
+def convert_to_binary(addy):
+	global address
+	address = "{:032b}".format(int(addy,16))
+	
 # function: reading cache
-#def read_cache(address):
-#	index = get_index(
+def read_cache(address):
+	
+
 
 #read input trace from txt file
 trace = open("word.txt", "r")
@@ -59,8 +80,10 @@ while i < numtrace:
 
 	data = read_trace.split();
 	# Converting addresses to binary 
-	address =  "{0:08b}".format(int(data[1],16))
+	convert_to_binary(data[1])
 	get_taglength(address)
+	print(data[1])
+	print(address)
 	print(taglength)	
 
 	if data[0] == "0":
@@ -73,5 +96,6 @@ while i < numtrace:
 		print("invalidation request");
 		
 	i = i + 1;
-	
+
+
 
